960802c2cc0b73ccf694684a7f2715eb2f490bf9
Jumair-aj <83821197+Jumair-aj@users.noreply.github.com>
2022-10-06 16:34:26 +0530

    Add files via upload

---
+++ src/components/Button/Button.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Button.css'
+import { Link } from 'react-router-dom'
+
+const STYLES = ['btn--primary','btn--outline']
+const SIZES = ['btn--medium','btn--large']
+
+function Button({
+    children,
+    onClick,
+    buttonSize,
+    buttonStyle,
+    type
+}) {
+
+    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0]
+    const checkButtonSize = STYLES.includes(buttonSize) ? buttonSize : SIZES[0]
+
+  return (
+      <Link to="/signup" className='btn-mobile'>
+
+      <button className={`btn ${checkButtonSize}
+      ${checkButtonStyle}`}
+      onClick = {onClick}
+      type={type}
+      >
+        {children}
+      </button>
+      </Link>
+  )
+}
+
+export default Button
